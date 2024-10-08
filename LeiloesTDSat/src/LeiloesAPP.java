ffa4f297b07ccd372427d6d01ed9a1dc24037284
maxzdosreis <maxzdosreis@gmail.com>
2024-10-08 16:23:04 -0300

    cadastroVIEW como tela principal funcionando

---
+++ LeiloesTDSat/src/LeiloesAPP.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author recep
+ */
+public class LeiloesAPP {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new cadastroVIEW().setVisible(true);
+            }
+        });
+    }
+
+}
